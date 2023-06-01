593505ce3da3f54fc9d71b4ff7b7ec46f204b414
supersliser <thomaslower9@gmail.com>
2023-06-01 11:42:34 +0100

    test

---
+++ src/scripts/test.js
@@ -0,0 +1,19 @@
+export default function afterMount() {
+    const elements = document.querySelectorAll(".anim");
+
+    function handleScroll() {
+        elements.forEach((element) => {
+            const elementTop = element.getBoundingClientRect().top;
+            const windowHeight = window.innerHeight;
+
+            if (elementTop < windowHeight) {
+                element.classList.add("fade-in");
+            }
+            else {
+                element.classList.remove("fade-in");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", handleScroll);
+}
