94a21e2181e20f3d7d818376c8c747c7b5dd1187
supersliser <thomaslower9@gmail.com>
2023-06-01 21:13:17 +0100

    jfdnjdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-
 import react from "@astrojs/react";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react()]
+  integrations: [react(), mdx()]
 });
