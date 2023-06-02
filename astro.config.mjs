e8d9201e528c36b8abf15019b117f979bfc864ac
supersliser <thomaslower9@gmail.com>
2023-06-02 23:11:30 +0100

    defaultSetup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/static";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercel(),
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), image()]
 });
